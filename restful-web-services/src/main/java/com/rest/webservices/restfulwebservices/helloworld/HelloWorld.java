0f2fa4fd29212cbeef011e42bb36bf7d55129aef
Pritesh Vasava <vasavapritesh356@gmail.com>
2024-01-21 01:57:46 +0530

    Class Hello world

--- restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorld.java
+++ restful-web-services/src/main/java/com/rest/webservices/restfulwebservices/helloworld/HelloWorld.java
@@ -1,5 +1,13 @@
 package com.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorld {
 
+	@RequestMapping("/hello-world")
+	public String Helloworld() {
+		return "Hello ";
+	}
 }
